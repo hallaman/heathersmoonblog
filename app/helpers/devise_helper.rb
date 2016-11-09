cdc769647e412d560957a83ad41fec6fdb8a4e6e
hallaman <heather.allaman@gmail.com>
2016-11-09 10:04:53 -0600

    remove flash timedout message

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -3,12 +3,15 @@ module DeviseHelper
     return '' if resource.errors.empty?
 
     messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    html = <<-HTML
-    <div class="alert alert-error alert-danger"> <button type="button"
-    class="close" data-dismiss="alert">×</button>
-      #{messages}
-    </div>
-    HTML
+
+    if value.is_a? String
+      html = <<-HTML
+        <div class="alert alert-<%= flash_class(level) %> fade in">
+          <button type="button" class="close" data-dismiss="alert">X</button>
+          #{messages}
+        </div>
+      HTML
+    end 
 
     html.html_safe
   end
