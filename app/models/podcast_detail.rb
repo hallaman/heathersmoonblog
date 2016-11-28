8bd0c0dc6acb3efc67baf329eae2577962ceaf22
hallaman <heather.allaman@gmail.com>
2016-11-28 13:27:55 -0600

    update to issues front end

--- app/models/podcast_detail.rb
+++ app/models/podcast_detail.rb
@@ -2,7 +2,9 @@ class PodcastDetail < ActiveRecord::Base
 	belongs_to :podcast, foreign_key: "podcast_id"
 
 	validates :title, presence: true
+	validates :link, format: { with: URI::regexp(%w(http https)),
+    message: "must begin with http:// or https://" }, unless: ->(podcast_detail){podcast_detail.entry.present?}
 
-  has_attached_file :image, styles: { thumb: ["200x200>", :png] }, :convert_options => { :thumb => "-quality 70 -interlace Plane" }
-  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  # has_attached_file :image, styles: { thumb: ["200x200>", :png] }, :convert_options => { :thumb => "-quality 70 -interlace Plane" }
+  # validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
--- app/models/sidebar.rb
+++ app/models/sidebar.rb
@@ -4,6 +4,7 @@ class Sidebar < ActiveRecord::Base
 	validates :title, presence: true
 	validates :link, format: { with: URI::regexp(%w(http https)),
     message: "must begin with http:// or https://" }, unless: ->(sidebar){sidebar.podcast_link_id.present?}
+
 	#has_attached_file :image, styles: { display: ["800x600>", :png], thumb: ["300x200>", :png] }, :convert_options => { :display => "-quality 70 -interlace Plane", :thumb => "-quality 70 -interlace Plane" }
   #validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
