989767b715a52ee68e87a01af05ace89cab6a255
hallaman <heather.allaman@gmail.com>
2016-12-05 10:08:16 -0600

    tryagain

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -44,8 +44,8 @@ class Issue < ActiveRecord::Base
   validates :main_image, presence: true
   validates :moon_phase, presence: true
 
-  has_attached_file :main_image, styles: {  thumb: ["400x300>", :png] }, :convert_options => { :thumb => "-quality 70 -interlace Plane" }
-  validates_attachment :main_image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  # has_attached_file :main_image, styles: {  thumb: ["400x300>", :png] }, :convert_options => { :thumb => "-quality 70 -interlace Plane" }
+  # validates_attachment :main_image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 
   def reject_day(attributes)
     attributes['date'].blank? && attributes['entry'].blank? && attributes['affirmation'].blank?
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -10,6 +10,6 @@ class Podcast < ActiveRecord::Base
 	validates :title, presence: true
   validates :main_image, presence: true
 
-  has_attached_file :main_image, styles: { thumb: ["400x300>", :png] }, :convert_options => { :thumb => "-quality 70 -interlace Plane" }
-  validates_attachment :main_image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  # has_attached_file :main_image, styles: { thumb: ["400x300>", :png] }, :convert_options => { :thumb => "-quality 70 -interlace Plane" }
+  # validates_attachment :main_image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
