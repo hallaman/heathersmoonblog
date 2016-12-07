2c2e8048b7e99f4644ece01ca4359312ed15ef07
hallaman <heather.allaman@gmail.com>
2016-12-07 16:56:27 -0600

    smtp;

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'aquariusnation.heroku.com' }
-
+  config.action_mailer.delivery_methode = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
