744776e75fdea598e5eece70427641c92b483070
hallaman <heather.allaman@gmail.com>
2016-12-07 16:29:05 -0600

    forms work

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ ruby '2.2.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 
-gem 'rails_12factor'
+#gem 'rails_12factor'
 
 # Use sqlite3 as the database for Active Record
 group :development, :test do
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.action_mailer.default_url_options = { :host => 'aquariusnation.heroku.com' }
+
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
