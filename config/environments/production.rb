46cc5f73b09364add68dc1fea2567e7e185eef33
hallaman <heather.allaman@gmail.com>
2016-12-07 17:03:13 -0600

    maybe work email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,14 +81,18 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => 'aquariusnation.heroku.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            'moonvibetribe@gmail.com',
-    password:             ENV['GMAIL_PASSWORD'],
-    authentication:       :plain,
-    enable_starttls_auto: true
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'gmail.com',
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: 'moonvibetribe@gmail.com',
+    password: ENV["GMAIL_PASSWORD"]
   }
 
   config.paperclip_defaults = {
