644e766937f7e8f2a12230443d2f9796512c31d1
hallaman <heather.allaman@gmail.com>
2017-01-13 10:58:56 -0600

    daily on home

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
   	# daily reading
   	@daily_reading = Post.where(category: 'MVG Daily Reading').where('publish_date BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).first
 
-    if @daily_reading.body.present?
+    if @daily_reading.present?
       @daily_reading_text = @daily_reading.body
     else 
       @stay_tuned = '<p>Please stay tuned.</p>'
