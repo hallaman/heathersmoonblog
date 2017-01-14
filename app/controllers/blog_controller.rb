55b6e6c425d2de8fb6b940e7e9eb5ff1054f496f
hallaman <heather.allaman@gmail.com>
2017-01-13 18:42:19 -0600

    show only blogs posted for now

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -3,7 +3,7 @@ class BlogController < ApplicationController
   layout 'application'
 
   def index
-    @posts = Post.all.where(category: 'Blog Post')
+    @posts = Post.all.where(category: 'Blog Post').where('publish_date <= ?', Time.now )
   end
 
 end
