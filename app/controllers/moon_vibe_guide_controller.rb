d451fdaa1aa4f6ea08e704fb62043365a128ca59
hallaman <heather.allaman@gmail.com>
2017-01-14 12:21:33 -0600

    daily reading has comments

--- app/controllers/moon_vibe_guide_controller.rb
+++ app/controllers/moon_vibe_guide_controller.rb
@@ -26,12 +26,10 @@ class MoonVibeGuideController < ApplicationController
   def daily_reading
     @daily_reading = Post.where(category: 'MVG Daily Reading').where('publish_date BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).first
 
-    @daily_reading_text = @daily_reading.present? ? @daily_reading.body : 'Please stay tuned.'
-
-    if @daily_reading.body.present?
+    if @daily_reading.present?
       @daily_reading_text = @daily_reading.body
     else 
-      @stay_tuned = '<p>Please stay tuned.</p>'
+      @stay_tuned = 'Please stay tuned.'
     end
 
   end
