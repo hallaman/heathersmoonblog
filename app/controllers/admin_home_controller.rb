11c058bf569042c63171d1cf79eff9187f0862ac
hallaman <heather.allaman@gmail.com>
2017-01-24 15:21:07 -0600

    mobile styling of home;

--- app/controllers/admin_home_controller.rb
+++ app/controllers/admin_home_controller.rb
@@ -2,7 +2,7 @@ class AdminHomeController < ApplicationController
 	before_action :authenticate_admin!
 
   def index
-    @newsbar = Newsbar.where(show: true).last
+    @newsbar = Newsbar.last
     @video = Video.where(show: true).last
 
     @latestitems = LatestItem.where(show: true).limit(3)
