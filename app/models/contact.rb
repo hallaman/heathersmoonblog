9ec5e0b2614aa3c0284c87a676ff9081ed4f4033
hallaman <heather.allaman@gmail.com>
2021-02-26 13:06:03 -0600

    removing old assets

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -8,7 +8,7 @@ class Contact < MailForm::Base
   # in ActionMailer accepts.
   def headers
     {
-      :subject => "Aquarius Nation Contact Form",
+      :subject => "Moon Blog Contact Form",
       :to => "heather.allaman@gmail.com",
       :from => %("#{name}" <#{email}>)
     }
