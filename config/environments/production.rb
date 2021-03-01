cc5e644ca0375b4f5d14dbb4e07c9aa8a90579ac
hallaman <heather.allaman@gmail.com>
2021-03-01 15:30:45 -0600

    prod settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,9 +81,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  Rails.application.routes.default_url_options[:host] = 'aquariusnation.heroku.com'
+  Rails.application.routes.default_url_options[:host] = 'heathersmoonblog.heroku.com'
 
-  config.action_mailer.default_url_options = { :host => 'aquariusnation.heroku.com' }
+  config.action_mailer.default_url_options = { :host => 'heathersmoonblog.heroku.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
